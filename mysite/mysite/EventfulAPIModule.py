1220ae9dd4f47ad023fdaa73200c837d9e64a92e
richmondr <richmond486@gmail.com>
2017-01-21 23:06:46 -0800

    Modified Eventful API Module to disregard events with duplicate locations

--- mysite/mysite/EventfulAPIModule.py
+++ mysite/mysite/EventfulAPIModule.py
@@ -23,12 +23,18 @@ def get_dict_from_json(url: str) -> dict:
             
 def get_results(json_text: 'json text') -> []:
     result_list = []
+    address_list = []
     
     for event in range(len(json_text['events']['event'])):
-        if json_text['events']['event'][event]['image'] == None:
-            result_list.append({'Title': json_text['events']['event'][event]['title'], 'Address': json_text['events']['event'][event]['venue_address'], 'EventURL': json_text['events']['event'][event]['url'], 'Date': json_text['events']['event'][event]['start_time'], 'EventImageURL': None})
+        if json_text['events']['event'][event]['venue_address'] in address_list:
+            pass
         else:
-            result_list.append({'Title': json_text['events']['event'][event]['title'], 'Address': json_text['events']['event'][event]['venue_address'], 'EventURL': json_text['events']['event'][event]['url'], 'Date': json_text['events']['event'][event]['start_time'], 'EventImageURL': json_text['events']['event'][event]['image']['medium']['url']})
+            if json_text['events']['event'][event]['image'] == None:
+                result_list.append({'Title': json_text['events']['event'][event]['title'], 'Address': json_text['events']['event'][event]['venue_address'], 'EventURL': json_text['events']['event'][event]['url'], 'Date': json_text['events']['event'][event]['start_time'], 'EventImageURL': None})
+                address_list.append(json_text['events']['event'][event]['venue_address'])
+            else:
+                result_list.append({'Title': json_text['events']['event'][event]['title'], 'Address': json_text['events']['event'][event]['venue_address'], 'EventURL': json_text['events']['event'][event]['url'], 'Date': json_text['events']['event'][event]['start_time'], 'EventImageURL': json_text['events']['event'][event]['image']['medium']['url']})
+                address_list.append(json_text['events']['event'][event]['venue_address'])
     
     for result in range(len(result_list)):
         if len(result_list[result]['Title']) >= 20:
@@ -42,5 +48,5 @@ def get_results(json_text: 'json text') -> []:
          
          
 # Test
-# print(build_url('Irvine, CA'))   
-# print(get_results(get_dict_from_json(build_url('Irvine, CA'))))
+# print(build_url('Los Angeles, CA'))   
+# print(get_results(get_dict_from_json(build_url('Los Angeles, CA'))))
